a14f9a4c194b4a971c331d51577338101c071860
Angelika2512 <andziaw2512@gmail.com>
2022-09-19 21:03:45 +0200

    added new clas Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hallo, world!");
+    public static void main(String[] args){
+        Scanner scanner; + new Scanner(System.in);
+        System.out.println("Wprowadz wartosc x");
+        int x = scanner.nextInt();
+        if (x<3) {
+            System.out.println("X jest mniejsze od 3");
+        }else {
+            System.out.println("X jest wieksze od3");
+        }
+        }
     }
-}
